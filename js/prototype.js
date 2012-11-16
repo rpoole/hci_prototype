2080f3c63139fd94b9e86ab490dd4b1718922937
Robert Poole <rbpl4328@gmail.com>
2012-11-15 22:05:25 -0500

    commmmmmmmitttingggg

--- js/prototype.js
+++ js/prototype.js
@@ -1,5 +1,12 @@
 grayedOut = false
 
+function filter_table(filter_text){
+  $("td").each(function() {
+    if ($(this).text() == filter_text) 
+        $(this).closest('tr').toggle();
+  });
+}
+
 function grayOut(vis, options) {
   // Pass true to gray out screen, false to ungray
   // options are optional.  This is a JSON object with the following (optional) properties
@@ -11,7 +18,7 @@ function grayOut(vis, options) {
   // in any order.  Pass only the properties you need to set.
   var options = options || {}; 
   var zindex = options.zindex || 50;
-  var opacity = options.opacity || 70;
+  var opacity = options.opacity || 90;
   var opaque = (opacity / 100);
   var bgcolor = options.bgcolor || '#000000';
   var dark=document.getElementById('darkenScreenObject');
@@ -21,45 +28,45 @@ function grayOut(vis, options) {
     // If you are getting errors in IE see: http://support.microsoft.com/default.aspx/kb/927917
     var tbody = document.getElementsByTagName("body")[0];
     var tnode = document.createElement('div');           // Create the layer.
-        tnode.style.position='absolute';                 // Position absolutely
-        tnode.style.top='0px';                           // In the top
-        tnode.style.left='0px';                          // Left corner of the page
-        tnode.style.overflow='hidden';                   // Try to avoid making scroll bars            
-        tnode.style.display='none';                      // Start out Hidden
-        tnode.id='darkenScreenObject';                   // Name it so we can find it later
+    tnode.style.position='absolute';                 // Position absolutely
+    tnode.style.top='0px';                           // In the top
+    tnode.style.left='0px';                          // Left corner of the page
+    tnode.style.overflow='hidden';                   // Try to avoid making scroll bars            
+    tnode.style.display='none';                      // Start out Hidden
+    tnode.id='darkenScreenObject';                   // Name it so we can find it later
     tbody.appendChild(tnode);                            // Add it to the web page
     dark=document.getElementById('darkenScreenObject');  // Get the object.
   }
   if (vis) {
     // Calculate the page width and height 
     if( document.body && ( document.body.scrollWidth || document.body.scrollHeight ) ) {
-        var pageWidth = document.body.scrollWidth+'px';
-        var pageHeight = document.body.scrollHeight+'px';
+      var pageWidth = document.body.scrollWidth+'px';
+      var pageHeight = document.body.scrollHeight+'px';
     } else if( document.body.offsetWidth ) {
       var pageWidth = document.body.offsetWidth+'px';
       var pageHeight = document.body.offsetHeight+'px';
     } else {
-       var pageWidth='100%';
-       var pageHeight='100%';
+      var pageWidth='100%';
+      var pageHeight='100%';
     }   
     //set the shader to cover the entire page and make it visible.
     dark.style.opacity=opaque;                      
     dark.style.MozOpacity=opaque;                   
     dark.style.filter='alpha(opacity='+opacity+')'; 
-    dark.style.zIndex=zindex;        
-    dark.style.backgroundColor=bgcolor;  
-    dark.style.width= pageWidth;
-    dark.style.height= pageHeight;
-    dark.style.display='block';				 
-  } else {
-     dark.style.display='none';
-  }
+        dark.style.zIndex=zindex;        
+        dark.style.backgroundColor=bgcolor;  
+        dark.style.width= pageWidth;
+        dark.style.height= pageHeight;
+        dark.style.display='block';				 
+        } else {
+          dark.style.display='none';
+        }
 
-  $('#darkenScreenObject').click(function() {
-    grayedOut = false;
-    grayOut(grayedOut);
-  });
-}
+        $('#darkenScreenObject').click(function() {
+          grayedOut = false;
+          grayOut(grayedOut);
+        });
+        }
 
 $(document).ready(function(){
   $('#dimmer').click(function() {
@@ -73,6 +80,12 @@ $(document).ready(function(){
     setTimeout(function() {
       $('#save-btn').button('reset');
     }, 2000);
+  });
+
+  $('.filter-btn').click(function() {
+   var filter_txt = $(this).text(); 
+   filter_table(filter_txt);
+
   });
 
   $('.item-modal').click(function() {
